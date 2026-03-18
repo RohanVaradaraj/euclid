e4ab45988f6c5deaf38537e44b6d7fbeb5c2198c
rohanvaradaraj <rohanva@gmail.com>
2026-03-18 14:33:57 +0000

    code to swarp LSST DP1 in CDFS

---
+++ src/file_management/swarp_lsst.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+"""
+swarp.py
+
+Swarp together the LSST tiles to make a full mosaic, resampled to VISTA and on same plate scale.
+
+Adapted from swarp_fornax.py: Friday 13th March 2026.
+"""
+
+import os
+from astropy.io import fits
+import glob
+from pathlib import Path
+import pickle
+import numpy as np
+from astropy.wcs import WCS
+import re
+
+#! Switches/variables
+filter_names = ['u', 'g', 'r', 'i', 'z', 'y'] #? LSST filter(s) we want to resample/swarp
+video_tiles = ['CDFS1', 'CDFS2', 'CDFS3'] #? VIDEO tile(s) we want to match the Euclid mosaics to.
+test = False #? If true, run on three tiles only
+
+# Path to the Euclid tiles
+lsst_dir = Path.home().parents[1] / 'extraspace' / 'varadaraj' / 'lsst'
+
+# Output directory
+output_dir = lsst_dir / 'mosaics'
+
+#! Load the Euclid tile names that lie in the VIDEO footprints.
+lsst_in_video = pickle.load(open('lsst_DP1_within_video.pkl', 'rb')) #! DR1
+print(lsst_in_video)
+
+# Loop over VIDEO tiles
+for video_tile in video_tiles:
+
+    # update output dir
+    final_dir = output_dir / video_tile
+    print(f'Final mosaic will be in {final_dir}')
+
+    # Open the VIDEO tile
+    video_dir = Path.home().parents[1] / 'hoy' / 'VIDEO_FINAL'
+    video_name = f'{video_tile.lower()}_Y.fits'
+    with fits.open(video_dir / video_name) as hdul:
+
+        # Get pixel scale
+        pixel_scale = np.abs(hdul[0].header['CD1_1']) * 3600
+        print(f'Pixel scale for {video_tile} is {pixel_scale} arcsec/pixel')
+
+        # Get the image size
+        image_size = [hdul[0].header['NAXIS1'], hdul[0].header['NAXIS2']]
+        print(f'Image size for {video_tile} is {image_size[0]},{image_size[1]} pixels')
+
+        # Find the centre of the image in ra dec
+        wcs = WCS(hdul[0].header)
+        centre = wcs.pixel_to_world(image_size[0] / 2, image_size[1] / 2)
+        print(f'Centre of {video_tile} is {centre.ra.deg},{centre.dec.deg}')
+
+    # Loop over Euclid filters
+    for filter_name in filter_names:
+
+        fits_files = glob.glob(str(lsst_dir / filter_name / '*SCI.fits'))  
+        rms_files = glob.glob(str(lsst_dir / filter_name / '*WHT.fits'))
+
+        # Get the tile numbers from the file names
+        all_tile_numbers = [filename.split('LSST_dp1_')[-1].split(f'_{filter_name}_SCI.fits')[0] for filename in fits_files]
+        all_tile_numbers_rms = [filename.split('LSST_dp1_')[-1].split(f'_{filter_name}_WHT.fits')[0] for filename in rms_files]
+        print(all_tile_numbers)
+        
+        #! Limit fits_files to those that lie in this specific VIDEO tile
+        fits_files = [f for f in fits_files if f.split('LSST_dp1_')[-1].split(f'_{filter_name}_SCI.fits')[0] in lsst_in_video[video_tile]]
+        rms_files = [f for f in rms_files if f.split('LSST_dp1_')[-1].split(f'_{filter_name}_WHT.fits')[0] in lsst_in_video[video_tile]]
+
+        #! Also limit the tile numbers to those that lie in this specific VIDEO tile
+        tile_numbers_here = [tile_number for tile_number in all_tile_numbers if tile_number in lsst_in_video[video_tile]]
+
+        # Build a dictionary of {tile_id: filename} for each
+        sci_dict = {filename.split('LSST_dp1_')[-1].split(f'_{filter_name}_SCI.fits')[0]: filename for filename in fits_files}
+        rms_dict = {filename.split('LSST_dp1_')[-1].split(f'_{filter_name}_WHT.fits')[0]: filename for filename in rms_files}
+
+        # Get intersection of available tile IDs in both lists
+        common_tile_ids = sorted(set(sci_dict) & set(rms_dict), key=int)
+
+        # Build aligned, ordered lists
+        fits_files = [sci_dict[tile_id] for tile_id in common_tile_ids]
+        rms_files = [rms_dict[tile_id] for tile_id in common_tile_ids]
+        
+        # Also sort the tile numbers!
+        tile_numbers_here = sorted(tile_numbers_here, key=lambda x: int(x))
+
+        if test:
+            # Limit to the first three tiles for testing
+            fits_files = fits_files[0:2]
+            rms_files = rms_files[0:2]
+            tile_numbers_here = tile_numbers_here[0:2]
+
+        #! ############################ OPTION B: SWARP ALL EUCLID TILES TOGETHER ############################
+        # Output file names
+        output_file = output_dir / video_tile / f'{video_tile}_LSST_{filter_name}_MOSAIC.fits'
+        outrms_file = output_dir / video_tile / f'{video_tile}_LSST_{filter_name}_MOSAIC_WHT.fits'
+
+        # Check if the output file and RMS file already exist
+        if output_file.exists():
+            print(f'Skipping {output_file} as it already exists.')
+            continue
+            
+        if outrms_file.exists():
+            print(f'Skipping {outrms_file} as it already exists.')
+            continue
+
+        #! If the file doesn't exist, go ahead and swarp it to match VIDEO!
+        # Key words
+        config_string = f'-c ./euclid_{video_tile.lower()}.swarp'
+        #config_string = f'-c ./euclid.swarp'
+
+        #! Run swarp on Euclid image
+        keywords = f'-WEIGHT_TYPE MAP_WEIGHT -COMBINE_TYPE WEIGHTED -VMEM_MAX 16384 -VMEM_DIR . \
+                    -MEM_MAX 2048 -COMBINE_BUFSIZE 2048 ' 
+
+        # Convert fits_files and rms_files to a comma-separated string
+        fits_files = ','.join(fits_files)
+        rms_files = ','.join(rms_files)
+        
+        swarp_command = f'~/swarp/bin/swarp {fits_files} -WEIGHT_IMAGE {rms_files} {config_string} -IMAGEOUT_NAME {output_file} {keywords}' 
+        print(swarp_command)
+        os.system(swarp_command)
+
+        #! And again on the weight image
+        keywords = "-WEIGHT_TYPE NONE \
+                -VMEM_MAX 16384  -VMEM_DIR . \
+                -MEM_MAX 2048 -COMBINE_BUFSIZE 2048 -COMBINE_TYPE AVERAGE "
+        
+        swarp_command_rms = f'~/swarp/bin/swarp {rms_files} {config_string} -IMAGEOUT_NAME {outrms_file} {keywords}'
+        print(swarp_command_rms)
+        os.system(swarp_command_rms)
